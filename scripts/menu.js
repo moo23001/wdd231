b1cd5646d06a51a528abd1d4962cf81c6e99cd4b
pedromoo <moo23001@byui.edu>
2024-09-18 18:18:58 -0600

    wayfining added

--- scripts/menu.js
+++ scripts/menu.js
@@ -5,3 +5,9 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('show');
 	hamButton.classList.toggle('show');
 });
+
+document.querySelectorAll('.navigation a').forEach(link => {
+    if (link.href === window.location.href) {
+        link.classList.add('active');
+    }
+});
