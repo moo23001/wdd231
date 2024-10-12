90fa136fb665fc7b98e896f58157e5bf34a3c234
pedromoo <moo23001@byui.edu>
2024-10-12 02:42:26 -0600

    Benefits cards, Join form updates

---
+++ chamber/scripts/join.js
@@ -0,0 +1,84 @@
+const url = './data/membership_benefits.json';
+const cards = document.querySelector('#membershipCards');
+const currentUrl = window.location.href;
+
+
+async function getMembershipData() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayMembershipCards(data);
+        } else {
+            console.error(`HTTP error! status: ${response.status}`);
+        }
+    } catch (error) {
+        console.error(`Fetch error: ${error}`);
+    }
+}
+
+getMembershipData();
+
+function displayMembershipCards (membership) {
+
+    membership.map((section) => {
+        const mCard = document.createElement('section');
+        mCard.innerHTML = `
+        <h3>${section.name}</h3>
+        <p>Learn More<p>
+    `;
+    mCard.classList.add('membershipCard')
+    cards.appendChild(mCard);
+    mCard.addEventListener("click", () => displayModal(section));
+    console.log(mCard)
+    })
+}
+
+function displayModal (membership) {
+    const learnMore = document.querySelector('.learnMore');
+    const closeButton = document.createElement('button');
+    const benefitsList = document.createElement('ul');
+
+
+    learnMore.innerHTML = '';
+    benefitsList.innerHTML = '';
+    closeButton.textContent = '❌';
+    const benefits = membership.benefits.map(benefit => `<li>${benefit}</li>`).join('');
+    benefitsList.innerHTML = `<ul>${benefits}<ul>`;
+
+    learnMore.appendChild(closeButton);
+    learnMore.appendChild(benefitsList);
+    
+    learnMore.showModal();
+
+    closeButton.addEventListener("click", () => {
+        learnMore.close();
+    })
+
+}
+
+const currentDateTime = new Date();
+document.querySelector('#timeStamp').value = currentDateTime;
+const urlAll = currentUrl.split('?');
+let formData = urlAll[1].split('&');
+
+function show(cup) {
+    formData.forEach((element) => {
+        if (element.startsWith(cup)) {
+            result = element.split('=')[1].replace("%40","@");
+        
+        }
+    })
+    return (result);
+}
+
+const showInfo = document.querySelector('.thankYou');
+showInfo.innerHTML = `
+<h1>Thank you for joining</h1>
+<h3>New Member<h3>
+<p>Name: ${show("first")} ${show("last")}</p>
+<p>Phone: ${show("phone")}</p>
+<p>Email: ${show("email")}</p>
+
+`;
